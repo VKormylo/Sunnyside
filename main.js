d9e0555c7040b626c93948f21a45f33d7e8d555d
VKormylo <vitaliykormylo@gmail.com>
2021-08-21 19:12:45 +0300

    update

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const menu = document.querySelector(".header-menu");
 const burger = document.querySelector(".burger-menu");
+const header = document.querySelector(".header-top");
+const about = document.querySelector(".features");
+const links = document.querySelectorAll(".header-menu__item");
+
+var pos =
+  about.getBoundingClientRect().top - document.body.getBoundingClientRect().top;
 
 burger.addEventListener("click", function () {
   menu.classList.toggle("visible");
+});
+
+window.addEventListener("scroll", function () {
+  if (document.documentElement.scrollTop > pos) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
 });
